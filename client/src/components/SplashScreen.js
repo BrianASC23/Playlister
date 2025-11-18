c1ab6857ceea502c609067fba85b88a97dce9bc9
Brian Cao <139932236+BrianASC23@users.noreply.github.com>
2025-11-17 19:28:48 -0500

    Removed the rickroll bg. Resdesigned the frontend of splashscreen using MUI components

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,91 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+
 export default function SplashScreen() {
     return (
-        <div id="splash-screen">
-            Playlister
-        </div>
+        <Box id="splash-screen"
+            sx={{
+                minHeight: "calc(100vh - 64px)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                bgcolor: "#f6f5dcff",
+                px: 2
+            }}
+        >
+            <Box sx={{alignItems: "center"}}>
+                <Typography
+                    component="h1"
+                    variant="h1"
+                    sx={{
+                        mb: 4,
+                        fontWeight: 500,
+                        color: "black"
+                    }}
+                >
+                    The Playlister
+                </Typography>
+                <Box
+                    component="img"
+                    src="/images/playlist-logo.png"
+                    alt="Playlister logo"
+                    sx={{
+                        width: 170,
+                        height: 170,
+                        objectFit: "contain",
+                        mb: 5,
+                    }}
+                >
+                </Box>
+                <Grid container spacing={3} justifyContent="center">
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                textTransform: "none",
+                                borderRadius: 1.5,
+                                px: 3,
+                                bgcolor: "black",
+                            }}
+                        >
+                            Continue As Guest
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                textTransform: "none",
+                                borderRadius: 1.5,
+                                px: 3,
+                                bgcolor: "black"
+                            }}
+                        >
+                            Login
+                        </Button>
+
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                textTransform: "none",
+                                borderRadius: 1.5,
+                                px: 3,
+                                bgcolor: "black"
+                            }}
+                        >
+                            Create Account
+                        </Button>
+
+                    </Grid>
+
+                </Grid>
+            </Box>
+        </Box>
     )
 }
